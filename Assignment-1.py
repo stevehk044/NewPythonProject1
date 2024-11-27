4707ee638218c5ad7d34cf68b5eb040c84f9a6f6
stevehk044 <stevehk044@gmail.com>
2024-11-26 19:27:38 -0500

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,22 @@
+#input display the three values
+principal = float(input("Enter the principal amount: "))
+
+rate = float(input("Enter the interest rate in percentage: "))
+
+time = float(input("Enter the time period in years: "))
+
+#Calculate the simple interest
+
+simpleInterest = (principal * rate * time) / 100
+print("this is calculated simple interest", simpleInterest)
+
+
+
+
+
+
+
+
+
+
+
